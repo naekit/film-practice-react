45f67dadf389c8255f997aba87215289f17024c0
naekit <mykyta.onishchuck@code.berlin>
2023-01-11 13:22:02 -0500

    transform data

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ function App() {
 	async function fetchHandler() {
 		const res = await fetch("https://swapi.dev/api/films/")
 		const data = await res.json()
-		setMovieArray(data.results)
+		const transformedMovieArray = data.results.map((movie) => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				openingText: movie.opening_crawl,
+				releaseDate: movie.release_date,
+			}
+		})
+		setMovieArray(transformedMovieArray)
 	}
 
 	return (
 		<React.Fragment>
 			<section>
-				<button onClick={() => fetchHandler()}>Fetch Movies</button>
+				<button onClick={fetchHandler}>Fetch Movies</button>
 			</section>
 			<section>
 				{movieArray.length > 1 && <MoviesList movies={movieArray} />}
