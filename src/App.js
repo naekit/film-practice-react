0e2138f9b27b1703e632a4bde2dc584c05fa8650
naekit <mykyta.onishchuck@code.berlin>
2023-01-11 16:03:53 -0500

    add item component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 
 import MoviesList from "./components/MoviesList"
 import "./App.css"
@@ -6,35 +6,56 @@ import "./App.css"
 function App() {
 	const [movieArray, setMovieArray] = useState([])
 	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
 
-	async function fetchHandler() {
+	const fetchHandler = useCallback(async () => {
 		setLoading(true)
-		const res = await fetch("https://swapi.dev/api/films/")
-		const data = await res.json()
-		const transformedMovieArray = data.results.map((movie) => {
-			return {
-				id: movie.episode_id,
-				title: movie.title,
-				openingText: movie.opening_crawl,
-				releaseDate: movie.release_date,
+		setError(null)
+		try {
+			const res = await fetch("https://swapi.dev/api/films/")
+
+			if (!res.ok) {
+				throw new Error(`Something went wrong!`)
 			}
-		})
-		setMovieArray(transformedMovieArray)
+
+			const data = await res.json()
+
+			const transformedMovieArray = data.results.map((movie) => {
+				return {
+					id: movie.episode_id,
+					title: movie.title,
+					openingText: movie.opening_crawl,
+					releaseDate: movie.release_date,
+				}
+			})
+			setMovieArray(transformedMovieArray)
+		} catch (error) {
+			setError(error.message)
+		}
 		setLoading(false)
-	}
+	}, [])
+
+	useEffect(() => {
+		fetchHandler()
+	}, [fetchHandler])
+
+	const content =
+		!loading && movieArray.length > 0 ? (
+			<MoviesList movies={movieArray} />
+		) : !loading && !error && movieArray.length === 0 ? (
+			<p>Found no movies</p>
+		) : !loading && error ? (
+			<p>{error}</p>
+		) : (
+			<p>Loading...</p>
+		)
 
 	return (
 		<React.Fragment>
 			<section>
 				<button onClick={fetchHandler}>Fetch Movies</button>
 			</section>
-			<section>
-				{!loading ? (
-					<MoviesList movies={movieArray} />
-				) : (
-					<p>Loading...</p>
-				)}
-			</section>
+			<section>{content}</section>
 		</React.Fragment>
 	)
 }
